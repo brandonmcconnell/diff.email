04fdd5245d2e7f111eb1cc7d4f132a9c57d2324f
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-06-30 04:10:18 -0400

    Remove duplicate browser_contexts database migrations

--- apps/server/src/db/migrations/0000_odd_stranger.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "browser_contexts" (
-	"id" text PRIMARY KEY NOT NULL,
-	"client" "client" NOT NULL,
-	"engine" "engine" NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL
-);
